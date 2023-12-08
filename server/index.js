ef956b56bfd1eeff361933640bda2402f63c0e0d
Shridhar Kandikatla <86877140+ShridharKandikatla@users.noreply.github.com>
2023-12-08 13:33:17 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -26,6 +26,10 @@ mongoose
     console.log(err.message);
   });
 
+app.get('/',(req,res)=>{
+  res.send('hello');
+})
+
 server = app.listen(process.env.PORT || 5000, () => {
   console.log('Server started on port 5000');
 });
