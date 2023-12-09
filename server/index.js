e9006a0442ae1e157a601e83d400c88e14190329
Shridhar Kandikatla <shridharkandikatla@gmail.com>
2023-12-09 12:34:59 +0530

    server updated

--- server/index.js
+++ server/index.js
@@ -8,11 +8,15 @@ const { default: mongoose } = require('mongoose');
 
 const app = express();
 app.use(express.json());
-app.use(cors({
-  origin:["https://chat-application-unpr.onrender.com",
-          "http://localhost:3000"],
-  credentials:true
-}));
+app.use(
+  cors({
+    origin: [
+      'https://chat-application-unpr.onrender.com',
+      'http://localhost:3000',
+    ],
+    credentials: true,
+  })
+);
 app.use('/user', userRoutes);
 app.use('/chat', chatRoutes);
 app.use('/message', messageRoutes);
@@ -26,9 +30,9 @@ mongoose
     console.log(err.message);
   });
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
   res.send('hello');
-})
+});
 
 server = app.listen(process.env.PORT || 5000, () => {
   console.log('Server started on port 5000');
@@ -53,8 +57,10 @@ io.on('connection', (socket) => {
   });
 
   socket.on('new message', (newMessageStatus) => {
-    if (!newMessageStatus.chat.users)
-      return console.log('chat.users not defined');
+    if (!newMessageStatus.chat.users) {
+      console.log('chat.users not defined');
+      return;
+    }
     newMessageStatus.chat.users.forEach((user) => {
       if (user._id == newMessageStatus.sender._id) return;
       socket.in(user._id).emit('message recieved', newMessageStatus);
